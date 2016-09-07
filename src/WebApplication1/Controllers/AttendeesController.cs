9c2eef2d4258fb843aeaf642940f0c838a591561
Scott Addie <tobias.addie@gmail.com>
2016-09-06 20:31:37 -0500

    Controller and view refactoring

--- src/WebApplication1/Controllers/AttendeesController.cs
+++ src/WebApplication1/Controllers/AttendeesController.cs
@@ -4,14 +4,13 @@ using Microsoft.Extensions.Options;
 using WebApplication1.Models;
 using WebApplication1.Services;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 namespace WebApplication1.Controllers
 {
     public class AttendeesController : Controller
     {
         private readonly IAttendeeService _attendeeService;
         private readonly IConfiguration _configuration;
-        private Attendee _attendee;
+        private readonly Attendee _attendee;
 
         public AttendeesController(IAttendeeService attendeeService,
                                    IOptions<Attendee> attendee,
@@ -19,27 +18,26 @@ namespace WebApplication1.Controllers
         {
             _attendeeService = attendeeService;
 
-            // Option 1: strongly-typed IOptions<T> approach
+            // APP SETTINGS APPROACH 1: strongly-typed IOptions<T>
             _attendee = attendee.Value;
 
-            // Option 2: IConfiguration.GetValue approach
+            // APP SETTINGS APPROACH 2: IConfiguration.GetValue
             _configuration = configuration;
         }
 
-        [HttpGet]
         public IActionResult Index()
         {
+            // APP SETTINGS APPROACH 1: strongly-typed IOptions<T>
+            var firstName = _attendee.FirstName;
+
+            // APP SETTINGS APPROACH 2: IConfiguration.GetValue
+            //var firstName = _configuration.GetValue<string>("AttendeeInfo:FirstName");
+
+            ViewBag.FirstName = firstName;
+
             var sessionList = _attendeeService.GetSessions();
 
             return View(sessionList);
         }
-
-        //public string AttendeeName()
-        //{
-        //    var firstName = _configuration.GetValue<string>("AttendeeInfo:FirstName");
-        //    var lastName = _configuration.GetValue<string>("AttendeeInfo:LastName");
-
-        //    return $"{firstName} {lastName}";
-        //}
     }
 }
